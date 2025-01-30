c4be87b200688d3551609718b1ee34cae08bfef7
Dmitriy Gnusov <dmitriy.gnusov@lemanapro.ru>
2025-01-30 13:18:12 +0300

    style: added storybook

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,5 +3,6 @@ import { render } from '@testing-library/react'
 import App from './App.tsx'
 
 test('should render', () => {
-  expect(() => render(<App />)).not.toThrow()
+  const result = render(<App />)
+  expect(result.queryByText('Vite + React')).to.toBeInTheDocument()
 })
